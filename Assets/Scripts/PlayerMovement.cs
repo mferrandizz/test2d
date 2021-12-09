a09cdf47a837bbd42c8692d5e773308f5767d7a6
mferrandizz <95858356+mferrandizz@users.noreply.github.com>
2021-12-09 13:19:24 +0100

    Update PlayerMovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,7 +7,7 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        console.WriteLine("Hola");
+        
     }
 
     // Update is called once per frame
